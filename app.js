cbf930e3fdf5805f859ac4f7b2c29b57ef8b75a9
Andrey Sergeyev <savilit@yandex.ru>
2023-04-29 19:33:22 +0300

    feat: Создать модели данных, соединение с БД и сервер

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+const { PORT = 3000, DB_PATH = 'mongodb://127.0.0.1:27017/mestodb' } =
+  process.env
+const app = express()
+
+app.use(bodyParser.json())
+app.use((req, res, next) => {
+  req.user = {
+    _id: '098765432211',
+  }
+  next()
+})
+
+mongoose.connect(DB_PATH)
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`)
+})
