028d6a257a8ce8fb6a65c6077fd72fc93802ff8f
Andrey Sergeyev <savilit@yandex.ru>
2023-05-14 17:54:05 +0300

    feat: добавить защиту от DDos и bruteforce

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const helmet = require('helmet')
 const cookieParser = require('cookie-parser')
 const { errors } = require('celebrate')
+const rateLimit = require('express-rate-limit')
 const routes = require('./routes')
 const auth = require('./middlewares/auth')
 const errorsHandler = require('./middlewares/handelError')
@@ -17,14 +18,24 @@ require('dotenv').config()
 
 const { PORT = 3000, DB_PATH = 'mongodb://127.0.0.1:27017/mestodb' } =
   process.env
+
+const authLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 5,
+  message:
+    'Too many register or login attempts from this IP, please try again after an hour',
+  standardHeaders: true,
+  legacyHeaders: false,
+})
+
 const app = express()
 
 app.use(helmet())
 app.use(bodyParser.json())
 app.use(cookieParser())
 
-app.post('/signin', validationLogin, login)
-app.post('/signup', validationCreateUser, createUser)
+app.post('/signin', [validationLogin, authLimiter], login)
+app.post('/signup', [validationCreateUser, authLimiter], createUser)
 
 app.use(auth)
 app.use(routes)
